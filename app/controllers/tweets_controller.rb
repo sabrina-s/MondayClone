2676805bfd0c291dbd81f7b125ebdcfddc0f9b74
sabrina-s <tesmemepasla@gmail.com>
2017-05-31 13:27:25 +0800

    Generate tweet model, controller, and view.

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,6 @@
+class TweetsController < ApplicationController
+  def main
+  end
+
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530074006) do
+ActiveRecord::Schema.define(version: 20170531052212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 20170530074006) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
