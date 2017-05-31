df1c0ee8ce179a3cd9a46ce649d70330c1f5c29d
wlteo <xeniel@gmail.com>
2017-05-31 23:23:47 +0800

    Add some controller actions for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,14 @@
 class TweetsController < ApplicationController
   def main
-    
+    @user = current_user
+    @tweet = @user.tweets.build
   end
 
   def new
   end
 
   def create
+
   end
 
   def show
