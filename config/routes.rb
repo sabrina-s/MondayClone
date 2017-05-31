23359ef07ce95a134d97e7ce975e4f874a2c0eab
sabrina-s <tesmemepasla@gmail.com>
2017-05-31 13:18:37 +0800

    Set resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'tweets#main'
+
+  resources :user do
+    resources :displaypic
+    # , only: [:show, :new, :create] do
+    #   member do
+    #     patch :set_as_primary
+    #   end
+    # end
+  end
+
+  resources :tweets
+  # , only: [:show] do
+  #   resource :picture, only: [:show, :new, :create]
+  # end
+
 end
