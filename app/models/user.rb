578fb8e5e76620dad6110f736a332f77db1154e3
sabrina-s <tesmemepasla@gmail.com>
2017-05-31 17:39:55 +0800

    Add rspec and shoulda matchers tests for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :tweets
+
+  validates_presence_of :username, :email, :password 
 
 end
---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Tweet, type: :model do
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:user_id) }
+  it { should belong_to(:user) }
+end
