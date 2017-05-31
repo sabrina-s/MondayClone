dac2b220fb18307b65f26917acb71e77d7353aad
wlteo <xeniel@gmail.com>
2017-05-31 22:35:11 +0800

    Allow email as permitted parameter to fix signup bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :password, :email])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 end
