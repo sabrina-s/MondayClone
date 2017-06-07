e53cab4796ca828b4998ce0b4abe88688343b19b
sabrina-s <tesmemepasla@gmail.com>
2017-06-07 14:04:00 +0800

    Update likes_controller, reverse order of tweets in tweets/home

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,10 @@
 class LikesController < ApplicationController
   def create
     @tweet = Tweet.find(params[:id])
-    @like = current_user.likes.build(@tweet)
+    @like = current_user.likes.build(:tweet_id => tweet_id[:id])
     @like.save
 
     redirect_to root_path
-    #
-    # if Like.find_by(tweet_id: @tweet.id, user_id: current_user) != nil
-    #   @like = Like.find_by(tweet_id: @tweet.id, user_id: current_user)
-    #   redirect_to like_path(@like.id, method: :delete)
-    # else
-    #   @like = @tweet.likes.build(user_id: current_user.id)
-    #   if @like.save
-    #     redirect_to root_path
-    #   else
-    #     redirect_to root_path
-    #   end
-    # end
   end
 
   def destroy
@@ -25,6 +13,12 @@ class LikesController < ApplicationController
     @like.delete
 
     redirect_to root_path
+  end
+
+  private
+
+  def tweet_id
+    params.permit(:id)
   end
 
 end
