0fedb5a89273e364ebd4ba26544e06c70438f488
lerhhl <lerhhl@gmail.com>
2017-06-14 11:32:41 +0800

    Create feature-show reminding char left for tweetbox

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,9 +1,21 @@
 function updateCountdown() {
-  var remaining = 140 - $('.form-control').val().length;
+  var remaining = 140 - $('#tweetbox').find("textarea").val().length;
   $('.countdown').text(remaining + ' characters remaining.');
 };
-// function updateCountdown() {
-//     // 140 is the max message length
-//     var remaining = 140 - jQuery('.message').val().length;
-//     jQuery('.countdown').text(remaining + ' characters remaining.');
-// }
+
+function button() {
+    if (remaining < 0) {
+    $(".create-button").html("<button class='btn btn-sucess pull-right'> Create </button>");
+  } else {
+    $(".create-button").html("<%= t.submit 'Create', class: 'btn btn-success pull-right'%>");
+    $(".tweetbox").html("<%= render partial: 'tweetbox' %>");
+  }
+};
+
+$(document).on("turbolinks:load", function() {
+    updateCountdown();
+    $(".form-control").keyup(updateCountdown);
+    $(".form-control").keydown(updateCountdown);
+});
+
+  
