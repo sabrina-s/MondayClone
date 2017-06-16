4ad47950958e67694dd6610308e3e63d088622c6
wlteo <xeniel@gmail.com>
2017-06-16 14:31:20 +0800

    Clean up user.rb model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:tweets) }
   it { should have_many(:likes) }
-  it { should validate_presence_of(:username) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
   it do
     should validate_length_of(:username).
     is_at_least(6)
